ccc9e0d8479655f5a8435f8fc0320dd410fdd2ef
Kim <phm2530@gmail.com>
2024-08-13 13:15:06 +0900

    first commit

---
+++ src/main/java/com/itwill/igojoamanagement/IgojoaManagementApplication.java
@@ -0,0 +1,13 @@
+package com.itwill.igojoamanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgojoaManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgojoaManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itwill/igojoamanagement/IgojoaManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itwill.igojoamanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgojoaManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
