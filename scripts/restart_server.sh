a178705bedc3a614e1928d08f0a9a09e25c2eadb
Kim <phm2530@gmail.com>
2024-08-13 16:48:15 +0900

    end.

--- scripts/restart_server.sh
+++ scripts/restart_server.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 # Navigate to the project's root directory
-cd  /home/ubuntu/igojoaManagement-0.0.1-SNAPSHOT.jar
+cd /home/ubuntu
 
 # Create the scripts directory
 mkdir -p scripts
@@ -7,7 +9,7 @@ mkdir -p scripts
 # Create the restart_server.sh script
 echo '#!/bin/bash
 pkill -f "java -jar"
-nohup java -jar /home/ubuntu/igojoaManagement-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &' > scripts/restart_server.sh
+nohup java -jar /home/ubuntu/igojoaManagement-0.0.1-SNAPSHOT.jar > /home/ubuntu/logs/nohup.out 2>&1 &' > scripts/restart_server.sh
 
 # Make the script executable
 chmod +x scripts/restart_server.sh
