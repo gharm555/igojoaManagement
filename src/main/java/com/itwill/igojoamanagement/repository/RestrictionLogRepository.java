a8ec3be5fa1699c38f261002d4ae75a19b7d3f5c
johnk <rnjsdygks12@gmail.com>
2024-09-02 18:11:17 +0900

    kyh

--- src/main/java/com/itwill/igojoamanagement/repository/RestrictionLogRepository.java
+++ src/main/java/com/itwill/igojoamanagement/repository/RestrictionLogRepository.java
@@ -1,10 +1,12 @@
 package com.itwill.igojoamanagement.repository;
 
+import com.itwill.igojoamanagement.domain.RestrictionLog;
+import com.itwill.igojoamanagement.domain.key.RestrictionLogPK;
 import com.itwill.igojoamanagement.domain.key.UserBlackListPK;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.itwill.igojoamanagement.domain.UserBlackList;
 
-public interface UserBlackListRepository extends JpaRepository<UserBlackList, UserBlackListPK>{
+public interface RestrictionLogRepository extends JpaRepository<RestrictionLog, RestrictionLogPK>{
 
 }
--- src/main/java/com/itwill/igojoamanagement/service/ReviewService.java
+++ src/main/java/com/itwill/igojoamanagement/service/ReviewService.java
@@ -1,15 +1,15 @@
 package com.itwill.igojoamanagement.service;
 
 import com.itwill.igojoamanagement.domain.ReportLog;
+import com.itwill.igojoamanagement.domain.RestrictionLog;
 import com.itwill.igojoamanagement.domain.Review;
-import com.itwill.igojoamanagement.domain.UserBlackList;
+import com.itwill.igojoamanagement.domain.key.RestrictionLogPK;
 import com.itwill.igojoamanagement.domain.key.ReviewPK;
-import com.itwill.igojoamanagement.domain.key.UserBlackListPK;
 import com.itwill.igojoamanagement.dto.ReportReviewDetailDto;
 import com.itwill.igojoamanagement.dto.ReportReviewDto;
 import com.itwill.igojoamanagement.repository.ReportLogRepository;
 import com.itwill.igojoamanagement.repository.ReviewRepository;
-import com.itwill.igojoamanagement.repository.UserBlackListRepository;
+import com.itwill.igojoamanagement.repository.RestrictionLogRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -27,7 +27,7 @@ public class ReviewService {
 
     private final ReviewRepository reviewRepository;
     private final ReportLogRepository reportLogRepository;
-    private final UserBlackListRepository userBlackListRepository;
+    private final RestrictionLogRepository restrictionLogRepository;
 
     @Transactional(readOnly = true)
     public Page<Review> findInappropriateReviews(Pageable pageable) {
@@ -51,8 +51,8 @@ public class ReviewService {
         ReportLog reportLog = reportLogRepository.findById(logId).orElseThrow();
         ReviewPK review = new ReviewPK(reportLog.getPlaceName(), reportLog.getReportedId());
 
-        // 블랙리스트에 등록
-        addBlackList(reportLog.getReportedId());
+        // 제재 내역에 등록
+        addRestrictionLog(reportLog);
 
         // 리뷰 테이블에서 삭제
         reviewRepository.deleteById(review);
@@ -74,18 +74,29 @@ public class ReviewService {
         reviewRepository.deleteById(reviewId);
     }
 
-    // 블랙리스트에 등록
+    // 제재 내역에 등록
     @Transactional
-    public void addBlackList(String userId) {
+    public void addRestrictionLog(ReportLog reportLog) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String adminId = authentication.getName();
 
-        UserBlackListPK blackListLog = new UserBlackListPK(userId);
-        UserBlackList userBlackList = UserBlackList.builder().userBlackListPK(blackListLog).reasonCode(101).adminId(adminId).build();
+        RestrictionLogPK restrictionLogPK = new RestrictionLogPK(reportLog.getReportedId());
 
-       userBlackListRepository.save(userBlackList);
+        // 신고 사유에 따라 detail (리뷰, 닉네임)
+        if (reportLog.getReasonCode() == 101) {
+            RestrictionLog restrictionLog = RestrictionLog.builder()
+                    .restrictionLogPK(restrictionLogPK)
+                    .adminId(adminId).reasonCode(reportLog.getReasonCode()).detail(reportLog.getReview()).build();
+            restrictionLogRepository.save(restrictionLog);
+        } else {
+            RestrictionLog restrictionLog = RestrictionLog.builder()
+                    .restrictionLogPK(restrictionLogPK)
+                    .adminId(adminId).reasonCode(reportLog.getReasonCode()).detail(reportLog.getReportedNickname()).build();
+            restrictionLogRepository.save(restrictionLog);
+        }
+
+
     }
-
     // 신고 리뷰 상세
     public ReportReviewDetailDto findReportReviewDetail(String logId) {
         ReportReviewDetailDto reviewDetail = reviewRepository.findReviewDetail(logId);
