30fa0bbdbb5e4e3b184af09969bedd11d7ef101e
soochjabg <soochang94@naver.com>
2024-09-02 11:23:58 +0900

    asd

--- src/main/java/com/itwill/igojoamanagement/controller/AdminController.java
+++ src/main/java/com/itwill/igojoamanagement/controller/AdminController.java
@@ -1,18 +1,16 @@
 package com.itwill.igojoamanagement.controller;
 
 import com.itwill.igojoamanagement.domain.Admin;
-import com.itwill.igojoamanagement.dto.AdminDto;
 import com.itwill.igojoamanagement.service.AdminService;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
-import java.io.IOException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -27,19 +25,22 @@ public class AdminController {
 
         log.info("signIn");
     }
+
+
     @PostMapping("/signIn")
-    public void login(@RequestParam String adminId, @RequestParam String password,
-                      HttpSession session, HttpServletResponse response) throws IOException {
-
-        Admin admin = adminService.signIn(adminId, password);
-
-        if (adminId != null) {
+    public String login(@RequestParam String adminId, @RequestParam String password,
+                        HttpSession session, RedirectAttributes redirectAttributes) {
+        log.info("Login attempt for adminId: {}", adminId);
+        try {
+            Admin admin = adminService.signIn(adminId, password);
             session.setAttribute("authenticatedAdmin", admin);
-            response.sendRedirect("/"); // 로그인 성공 시 루트 경로로 리디렉션
-        } else {
-            response.sendRedirect("/signIn?error=true"); // 로그인 실패 시 로그인 페이지로 리디렉션
+            return "redirect:/index";
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("error", "로그인에 실패했습니다.");
+            return "redirect:/admin/signIn";
         }
     }
+
 
     @PostMapping("/signOut")
     public String signOut(HttpSession session) {
