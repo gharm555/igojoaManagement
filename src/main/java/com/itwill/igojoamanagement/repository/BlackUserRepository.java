ebc0fa58bee3d8293f71d210637a7aac51595e6c
johnk <rnjsdygks12@gmail.com>
2024-09-05 10:04:35 +0900

    kyh 09-05

--- src/main/java/com/itwill/igojoamanagement/repository/BlackUserRepository.java
+++ src/main/java/com/itwill/igojoamanagement/repository/BlackUserRepository.java
@@ -2,11 +2,6 @@ package com.itwill.igojoamanagement.repository;
 
 import com.itwill.igojoamanagement.domain.BlackUser;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlackUserRepository extends JpaRepository<BlackUser, String>{
-
-import com.itwill.igojoamanagement.domain.key.UserBlackListPK;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
