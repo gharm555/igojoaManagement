e17fbd599bdc3e6432aba66dc02f12cfe04cdcf7
johnk <rnjsdygks12@gmail.com>
2024-09-05 10:13:23 +0900

    kyh 09-05

--- src/main/java/com/itwill/igojoamanagement/repository/UserRepository.java
+++ src/main/java/com/itwill/igojoamanagement/repository/UserRepository.java
@@ -1,17 +1,11 @@
 package com.itwill.igojoamanagement.repository;
 
-import com.itwill.igojoamanagement.domain.ReportLog;
-import com.itwill.igojoamanagement.domain.RestrictionLog;
-import com.itwill.igojoamanagement.dto.UserDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.itwill.igojoamanagement.domain.User;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, String>{
     Page<User> findAll(Pageable pageable);
-
 }
